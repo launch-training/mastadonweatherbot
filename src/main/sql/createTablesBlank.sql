dc0ea5bee5e41a588ef9fec77ccb447d7f0aa01f
Alina Gruenaug <alina.gruenaug@accenture.com>
2024-07-30 16:16:42 +0200

    added createTablesBlank.sql

---
+++ src/main/sql/createTablesBlank.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `post_history` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `guid` varchar(45) NOT NULL,
+   `timestamp_weather_request` datetime NOT NULL,
+   `weather_api_response` json DEFAULT NULL,
+   `timestamp_mastodon_posted` datetime DEFAULT NULL,
+   `error_logging` varchar(100) DEFAULT NULL,
+   `location_id` int NOT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `guid_UNIQUE` (`guid`),
+   KEY `fk_location_id_idx` (`location_id`),
+   CONSTRAINT `fk_location_id` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `post_history` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `guid` varchar(45) NOT NULL,
+   `timestamp_weather_request` datetime NOT NULL,
+   `weather_api_response` json DEFAULT NULL,
+   `timestamp_mastodon_posted` datetime DEFAULT NULL,
+   `error_logging` varchar(100) DEFAULT NULL,
+   `location_id` int NOT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `guid_UNIQUE` (`guid`),
+   KEY `fk_location_id_idx` (`location_id`),
+   CONSTRAINT `fk_location_id` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
